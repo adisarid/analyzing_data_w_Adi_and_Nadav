888cfe1532a0cf47628f9607de4bb29d6ca3ccd4
Adi Sarid <adi@sarid-ins.co.il>
2022-07-06 09:43:17 +0300

    extracting week and year from file

--- s01e02_moh_epidemiology_reports/R/read_and_tidy.R
+++ s01e02_moh_epidemiology_reports/R/read_and_tidy.R
@@ -2,42 +2,18 @@
 
 library(tidyverse)
 
-read_and_tidy <- function(week_num, year, directory_listing = dir("data")){
+read_and_tidy <- function(filename){
   
-  # file name might be one of a few options, check which is it:
-  file_name_opt1 <- glue::glue("data/files_weekly-epidemiology_{year}_IWER_{stringr::str_pad(week_num, width = 2, pad = '0')}_{year}.xlsx")
-  file_name_opt2 <- glue::glue("data/files_weekly-epidemiology_{year}_IWER_{week_num}_{year}.xlsx")
-  file_name_opt3 <- glue::glue("data/files_weekly-epidemiology_{year}_IWER_{week_num}_{year}.xls")
-  file_name_opt4 <- glue::glue("data/files_weekly-epidemiology_{year}_IWER_{stringr::str_pad(week_num, width = 2, pad = '0')}_{year}.xls")
-  file_name_opt5 <- glue::glue("data/files_weekly-epidemiology_{year}_IWER{stringr::str_pad(week_num, width = 2, pad = '0')}_{year}.xlsx")
-  file_name_opt6 <- glue::glue("data/files_weekly-epidemiology_{year}_IWER{week_num}_{year}.xlsx")
-  file_name_opt7 <- glue::glue("data/files_weekly-epidemiology_{year}_IWER{week_num}_{year}.xls")
-  file_name_opt8 <- glue::glue("data/files_weekly-epidemiology_{year}_IWER{stringr::str_pad(week_num, width = 2, pad = '0')}_{year}.xls")
+  # extract year and week number
+  find_start <- str_locate(filename, "files_weekly-epidemiology_") # so that you can make do with any subdirectory length
+  year <- str_sub(filename, start = find_start[2]+1, end = find_start[2]+4)
+  week_num <- str_remove_all(str_sub(filename, start = find_start[2]+5),
+                             glue::glue("{year}|.xlsx|.xls|_|IWER"))
   
-  
-  
-  if (file_name_opt1 %in% directory_listing){
-    file_name <- file_name_opt1
-  } else if (file_name_opt2 %in% directory_listing){
-    file_name <- file_name_opt2
-  } else if (file_name_opt3 %in% directory_listing){
-    file_name <- file_name_opt3
-  } else if (file_name_opt4 %in% directory_listing){
-    file_name <- file_name_opt4
-  } else if (file_name_opt5 %in% directory_listing){
-    file_name <- file_name_opt5
-  } else if (file_name_opt6 %in% directory_listing){
-    file_name <- file_name_opt6
-  } else if (file_name_opt7 %in% directory_listing){
-    file_name <- file_name_opt7
-  } else if (file_name_opt8 %in% directory_listing){
-    file_name <- file_name_opt8
-  } else {
-    stop(glue::glue("Could not find file {year}-{week_num} in directory_listing"))
-  }
+  # read file
   
   file_data <- readxl::read_excel(
-    file_name,
+    filename,
     range = "A41:T128") %>% 
     rename(disease_type = `Week No.`,
            disease_type_heb = `שבוע מס'`) %>% 
